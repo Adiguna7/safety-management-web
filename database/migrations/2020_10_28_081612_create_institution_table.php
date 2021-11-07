8151650260c4c0e33debc60a72428a70121bf363
Suryo Adiguna <sevensag@gmail.com>
2021-11-07 08:00:21 +0700

    updated struktur db institusi

--- database/migrations/2020_10_28_081612_create_institution_table.php
+++ database/migrations/2020_10_28_081612_create_institution_table.php
@@ -17,7 +17,7 @@ class CreateInstitutionTable extends Migration
             $table->id();
             $table->string('institution_name');
             $table->enum('category', ['institution', 'company', 'umum', 'expert']);
-            $table->string('institution_code')->nullable();
+            $table->string('institution_code')->unique();
             $table->string('response')->default(0);
             $table->string('max_response');
             $table->timestamps();
